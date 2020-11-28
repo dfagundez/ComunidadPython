5cc55fab5dba8b09e605ae139c93b8cc94da1898
Diego Fagundez <dfagundezz@gmail.com>
2020-11-27 22:21:48 -0300

    Prmer ejercicio propuesto por FEC resuelto

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+# Python script by Diego Fagundez.
+
+def palindromo(sentencia):
+    sentenciaProcesada = sentencia.replace(" ", "").lower()
+    sentenciaProcesadaInvertida = sentenciaProcesada[::-1]
+    return print(sentenciaProcesada == sentenciaProcesadaInvertida)
+
+
+# Run script
+if __name__ == '__main__':
+    palindromo("Anita lava la tina")
+    palindromo("Sometamos o matemos")
+    palindromo("Super palindromo")
